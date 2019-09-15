87de790d2ad3a5596da409bbd58463ae4394bf07
doorBW <doorbw@outlook.com>
2019-09-15 18:53:44 +0900

    project done

--- ExcelCalculate/calculate/models.py
+++ ExcelCalculate/calculate/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Document(models.Model):
+    user_upload_file = models.FileField(upload_to='user_upload_files/%Y%m%d/')
--- ExcelCalculate/calculate/views.py
+++ ExcelCalculate/calculate/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import pandas as pd
+from datetime import datetime
+from .models import *
 
 # Create your views here.
 def calculate(request):
     file = request.FILES['fileInput']
     # print("# 사용자가 등록한 파일의 이름: ",file)
+
+    # 파일 저장하기
+    origin_file_name = file.name
+    user_name = request.session['user_name']
+    now_HMS = datetime.today().strftime('%H%M%S')
+    file_upload_name = now_HMS+'_'+user_name+'_'+origin_file_name
+    file.name = file_upload_name
+    document = Document(user_upload_file = file)
+    document.save()
+
     df = pd.read_excel(file,sheet_name='Sheet1',header=0)
     # print(df.head(5))
     # grade별 value리스트 만들기
